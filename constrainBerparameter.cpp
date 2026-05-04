489578bfc3d3eaa45940c7e3c605aebc16efefa3
Ilasbintang <ilasbintang727@gmail.com>
2026-05-04 18:38:46 +0700

    commit 6

--- constrainBerparameter.cpp
+++ constrainBerparameter.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class mahasiswa{
+    public:
+    //Construktor dengan parameter
+    mahasiswa(int nim, string nama);
 
+};
+//definisi Construktor dg parameter
 
 
